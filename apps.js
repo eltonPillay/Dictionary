174e95cbefb6ab00830cf85723e41230392e7606
eltonPillay <biduudywebsolutions@gmail.com>
2023-05-26 16:10:45 +0200

    added some functionality

--- apps.js
+++ apps.js
@@ -1,1 +1,13 @@
-console.log("ss");
+const apiurl = axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/hello')
+
+const but = document.getElementById("searchbut");
+
+but.addEventListener("click", function(event) {
+    event.preventDefault();
+    const search = document.getElementById("userin").value;
+    console.log(search);
+});
+
+
+
+

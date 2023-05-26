b93fcf340fa7462f178d3f4840ff302c855b3d07
eltonPillay <biduudywebsolutions@gmail.com>
2023-05-27 00:00:20 +0200

    added some js - alot more functionality to work out

--- apps.js
+++ apps.js
@@ -1,11 +1,44 @@
-const apiurl = axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/hello')
+/*const apiurl = axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/hello')*/
 
 const but = document.getElementById("searchbut");
 
-but.addEventListener("click", function(event) {
+but.addEventListener("click", async (event) => {
     event.preventDefault();
-    const search = document.getElementById("userin").value;
-    console.log(search);
+    try {
+        const search = document.getElementById("userin").value;
+        const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`);
+        const apiurl = response.data[0];
+        const pos = document.getElementById('postxt');
+        const def = document.getElementById('deftxt');
+        const word = document.getElementById('word');
+        word.innerHTML = search;
+
+        // Clear existing content in pos and def elements
+        pos.innerHTML = '';
+        def.innerHTML = '';
+
+        // Loop through meanings array and add partOfSpeech values to pos element
+        for (let i = 0; i < apiurl.meanings.length; i++) {
+            const meaning = apiurl.meanings[i];
+            const partOfSpeech = meaning.partOfSpeech;
+            const posItem = document.createElement('h2');
+            posItem.textContent = partOfSpeech;
+            posItem.classList.add('postxt');
+            pos.appendChild(posItem);
+            
+        
+            // Get the first definition for the current part-of-speech
+            const definition = meaning.definitions[0].definition;
+            const defItem = document.createElement('li');
+            defItem.textContent = definition;
+            def.appendChild(defItem);
+        }
+
+        console.log(search);
+        console.log(apiurl.meanings[0].partOfSpeech);
+    } catch (error) {
+        console.log("Error:", error);
+    }
 });
 
 
